74c7d9c5ecd605add5d310cb0fe16e11a78ac00b
ct0202 <arman.jumashev.dev@gmail.com>
2025-04-12 08:17:31 +0500

    huge

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = 'https://buziak.online'; 
-export const API_BASE_URL = 'http://localhost:5002';
+export const API_BASE_URL = 'https://buziak.online'; 
+// export const API_BASE_URL = 'http://localhost:5002';
